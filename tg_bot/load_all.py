c614d6806061483fe22e8135171bef355019fb7b
wityat <ttrttr449@gmail.com>
2020-12-02 23:00:19 +0300

    register custom_state filter

--- tg_bot/load_all.py
+++ tg_bot/load_all.py
@@ -6,6 +6,7 @@ from aiogram import Dispatcher
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
+from .modules.filters import CustomStateFilter
 from .modules.languages_middelware import setup_middleware
 from .modules.middlewares import GetUserMiddleware
 from .config import *
@@ -19,6 +20,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 dp.middleware.setup(LoggingMiddleware())
 dp.middleware.setup(GetUserMiddleware())
+dp.filters_factory.bind(CustomStateFilter, event_handlers=[dp.message_handlers, dp.callback_query_handlers])
 i18n = setup_middleware(dp)
 i18n.reload()
 
