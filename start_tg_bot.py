b90b5f7fffde00dc69dec6d20fe92f5d291f0291
wityat <ttrttr449@gmail.com>
2020-12-02 22:26:40 +0300

    fix bot start

--- start_tg_bot.py
+++ start_tg_bot.py
@@ -1,9 +1,7 @@
-import json
 from aiogram import executor
 from tortoise import Tortoise
 
-from tg_bot.load_all import bot, rabbit_donation_queue
-from tg_bot.modules.donation import process_donation
+from tg_bot.load_all import bot
 
 
 async def on_shutdown(dp):
@@ -17,6 +15,5 @@ async def on_startup(dp):
     await bot.send_message("446162145", "Bot is running!")
 
 
-from tg_bot.dialogs.users.handlers import dp
 from tg_bot.dialogs.general.handlers import dp
 executor.start_polling(dp, on_shutdown=on_shutdown, on_startup=on_startup, skip_updates=True)
