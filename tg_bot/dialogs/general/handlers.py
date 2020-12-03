08f0efc81223302f9558206047b7cdfde3a75ad8
wityat <ttrttr449@gmail.com>
2020-12-03 03:11:45 +0300

    change logic of edit_or_send_message.py + add set to None data

--- tg_bot/dialogs/general/handlers.py
+++ tg_bot/dialogs/general/handlers.py
@@ -50,6 +50,7 @@ async def set_action(callback: types.CallbackQuery, state: FSMContext, bot_user:
 @dp.callback_query_handler(Button("get_data:", True), state="*")
 async def get_data(callback: types.CallbackQuery, state: FSMContext, bot_user: BotUser):
     get_data_way = callback.data.split(":")[-1]
+    await state.update_data(get_empty_data())
     if get_data_way == "easy":
         await edit_or_send_message(bot, callback, state, text=texts.get_data_easy())
         await GetDataEasy.me.set()
