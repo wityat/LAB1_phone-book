8f392a87041dee926460586955689e4c628e5a83
wityat <ttrttr449@gmail.com>
2020-12-03 06:34:22 +0300

    back to date

--- tg_bot/modules/validation.py
+++ tg_bot/modules/validation.py
@@ -17,7 +17,8 @@ def validate_birth_day(bd: str) -> [str, None]:
         except Exception as e:
             raise ValidateError(exceptions_texts.bad_date())
         else:
-            return d.strftime("%Y-%m-%d")
+            # return d.strftime("%Y-%m-%d")
+            return d
     else:
         raise ValidateError(exceptions_texts.bad_date())
 
